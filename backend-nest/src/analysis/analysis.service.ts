a514d605ede80127075c6f9215407119d3b27e55
Deniz Demirören <denizdemiroren11@gmail.com>
2025-12-09 01:22:02 +0300

    fix: spdxCorrect import and improve license-checker optioasdans

--- backend-nest/src/analysis/analysis.service.ts
+++ backend-nest/src/analysis/analysis.service.ts
@@ -7,8 +7,7 @@ import { promisify } from 'util';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
-import * as spdxCorrect from 'spdx-correct';
-
+import spdxCorrect from 'spdx-correct';
 const execAsync = promisify(exec);
 
 @Injectable()
